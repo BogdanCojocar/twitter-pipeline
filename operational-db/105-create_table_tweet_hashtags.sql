2e6c1042bf92f26c582173dde9207a1942d071ca
BogdanCojocar <bogdan.cojocar@mydrivesolutions.com>
2016-07-18 21:21:31 +0100

    add okey constrint for the tweet_hashtags table

--- operational-db/105-create_table_tweet_hashtags.sql
+++ operational-db/105-create_table_tweet_hashtags.sql
@@ -1,4 +1,5 @@
 CREATE TABLE tweet_hashtags (
   tweet_id INT REFERENCES tweet(id) ON UPDATE CASCADE ON DELETE NO ACTION,
-  hashtag_id INT REFERENCES hashtags(id) ON UPDATE CASCADE ON DELETE NO ACTION
+  hashtag_id INT REFERENCES hashtags(id) ON UPDATE CASCADE ON DELETE NO ACTION,
+  CONSTRAINT tweet_hashtags_pkey PRIMARY KEY(tweet_id, hashtag_id)
 );
