f075674412d830f00269c881dc793f372449edaf
BogdanCojocar <bogdan.cojocar@mydrivesolutions.com>
2016-07-23 17:51:20 +0100

    updated tweet table to contain created_at field

--- operational-db/104-create_table_tweet.sql
+++ operational-db/104-create_table_tweet.sql
@@ -2,10 +2,11 @@ CREATE TABLE tweet (
   id INT NOT NULL,
   place_id VARCHAR(50) REFERENCES place(id) ON UPDATE CASCADE ON DELETE NO ACTION,
   user_id INT REFERENCES users(id) ON UPDATE CASCADE ON DELETE NO ACTION,
+  created_at TIMESTAMP WITHOUT TIME ZONE,
   lang VARCHAR(255),
   tweet_favorite_count INT,
   retweet_count INT,
   retweeted BOOLEAN,
   tweet TEXT,
-  PRIMARY KEY(id)
+  PRIMARY KEY(id, created_at)
 );
